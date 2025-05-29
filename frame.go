8157bf54a7480b59f2efff9d16cf80657c8413f7
sina.a.m <sinaaarabi2@gmail.com>
2025-05-29 18:20:06 +0330

    implements setting and header frame decoder

---
+++ frame.go
@@ -0,0 +1,267 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+)
+
+var (
+	ErrProtocol = errors.New("protocol error")
+)
+
+type (
+	FrameType    uint8
+	FlagType     uint8
+	SettingParam uint16
+)
+
+const (
+	DataFrameType    FrameType = 0x00
+	HeaderFrameType  FrameType = 0x01
+	SettingFrameType FrameType = 0x04
+
+	UnsetFlag           FlagType = 0x0
+	SettingAckFlag      FlagType = 0x1
+	HeaderEndStreamFlag FlagType = 0x1
+	HeaderEndHeaderFlag FlagType = 0x4
+	HeaderPaddingFlag   FlagType = 0x8
+	HeaderPriorityFlag  FlagType = 0x20
+
+	SettingsHeaderTableSize      SettingParam = 1 // 4,096
+	SettingsEnablePush           SettingParam = 2 // true
+	SettingsMaxConcurrentStreams SettingParam = 3 // no less than 100
+	SettingsInitialWindowSize    SettingParam = 4 // 65,535
+	SettingsMaxFrameSize         SettingParam = 5 // 16,384
+	SettingsMaxHeaderListSize    SettingParam = 6 // unlimited
+)
+
+/*
+All frames begin with a fixed 9-octet header followed by a variable-
+length payload.
+
+	+-----------------------------------------------+
+	|                 Length (24)                   |
+	+---------------+---------------+---------------+
+	|   Type (8)    |   Flags (8)   |
+	+-+-------------+---------------+-------------------------------+
+	|R|                 Stream Identifier (31)                      |
+	+=+=============================================================+
+	|                   Frame Payload (0...)                      ...
+	+---------------------------------------------------------------+
+
+	                      Figure 1: Frame Layout
+*/
+type Frame struct {
+	Type     FrameType
+	StreamID uint32
+	Data     any
+}
+
+/*
+The payload of a SETTINGS frame consists of zero or more parameters,
+each consisting of an unsigned 16-bit setting identifier and an
+unsigned 32-bit value.
+
+	+-------------------------------+
+	|       Identifier (16)         |
+	+-------------------------------+-------------------------------+
+	|                        Value (32)                             |
+	+---------------------------------------------------------------+
+
+	                     Figure 10: Setting Format
+*/
+type SettingFrame struct {
+	AckFlag FlagType
+	Params  map[SettingParam]uint32
+}
+
+/*
+Headers frame structure
+
+	+---------------+
+	|Pad Length? (8)|
+	+-+-------------+-----------------------------------------------+
+	|E|                 Stream Dependency? (31)                     |
+	+-+-------------+-----------------------------------------------+
+	|  Weight? (8)  |
+	+-+-------------+-----------------------------------------------+
+	|                   Header Block Fragment (*)                 ...
+	+---------------------------------------------------------------+
+	|                           Padding (*)                       ...
+	+---------------------------------------------------------------+
+
+	Figure 7: HEADERS Frame Payload
+*/
+type HeaderFrame struct {
+	EndStreamFlag FlagType
+	EndHeaderFlag FlagType
+	PaddingFlag   FlagType
+	PriorityFlag  FlagType
+
+	StreamDependency uint32
+	PaddingLength    uint8
+	Weight           uint8
+
+	HeaderFields []HeaderField
+}
+
+type frameHandler struct {
+	encoder HPackEncoder
+	decoder HPackDecoder
+}
+
+func NewFrameHandler() *frameHandler {
+	return &frameHandler{
+		encoder: NewHPackEncoder(),
+		decoder: NewHPackDecoder(),
+	}
+}
+
+func (h *frameHandler) Encode(writer io.Writer, frame Frame) (int, error) {
+	switch frame.Type {
+	case SettingFrameType:
+		settingFrame, ok := frame.Data.(SettingFrame)
+		if !ok {
+			return 0, fmt.Errorf("invalid data")
+		}
+
+		length := len(settingFrame.Params) * 6
+
+		packet := make([]byte, 9+length)
+		// Length (24)
+		packet[0] = byte((length >> 16) & 0xFF)
+		packet[1] = byte((length >> 8) & 0xFF)
+		packet[2] = byte(length & 0xFF)
+		packet[3] = byte(SettingFrameType)                     // Type (8)
+		packet[4] = byte(settingFrame.AckFlag)                 // Flags (8)
+		binary.BigEndian.PutUint32(packet[5:], frame.StreamID) // StreamID (32)
+
+		index := 9
+		for identifier, value := range settingFrame.Params {
+			binary.BigEndian.PutUint16(packet[index:], uint16(identifier)) // 9-11
+			binary.BigEndian.PutUint32(packet[index+2:], value)            // 11-15
+
+			index += 6
+		}
+
+		return writer.Write(packet)
+	case HeaderFrameType:
+		headerFrame, ok := frame.Data.(HeaderFrame)
+		if !ok {
+			return 0, fmt.Errorf("invalid data")
+		}
+
+		packet := make([]byte, 9)
+		packet[3] = byte(HeaderFrameType) // Type (8)
+		packet[4] = byte(
+			headerFrame.EndStreamFlag |
+				headerFrame.EndHeaderFlag |
+				headerFrame.PaddingFlag |
+				headerFrame.PriorityFlag,
+		) // Flags (8)
+		binary.BigEndian.PutUint32(packet[5:], frame.StreamID) // StreamID (32)
+
+		if headerFrame.PaddingFlag != UnsetFlag {
+			packet = append(packet, headerFrame.PaddingLength)
+		}
+		if headerFrame.PriorityFlag != UnsetFlag {
+			packet = binary.BigEndian.AppendUint32(packet, headerFrame.StreamDependency)
+			packet = append(packet, headerFrame.Weight)
+		}
+
+		buf := bytes.Buffer{}
+		_, err := h.encoder.Encode(&buf, headerFrame.HeaderFields)
+		if err != nil {
+			return 0, err
+		}
+
+		packet = append(packet, buf.Bytes()...)
+		length := len(packet) - 9
+		packet[0] = byte((length >> 16) & 0xFF)
+		packet[1] = byte((length >> 8) & 0xFF)
+		packet[2] = byte(length & 0xFF)
+
+		return writer.Write(packet)
+	default:
+		return 0, fmt.Errorf("invalid frame type")
+	}
+}
+
+func (h *frameHandler) Decode(reader io.Reader, frame *Frame) error {
+	packet := make([]byte, 9)
+	n, err := reader.Read(packet[:9])
+	if err != nil {
+		return err
+	}
+	if n != 9 {
+		return fmt.Errorf("invalid packet header")
+	}
+
+	frameLength := uint32(uint32(packet[0])<<16) + (uint32(packet[1]) << 8) + uint32(packet[2])
+	frame.Type = FrameType(packet[3])
+	frame.StreamID = binary.BigEndian.Uint32(packet[5:9])
+
+	// Resize packet size
+	packet = make([]byte, int(frameLength))
+
+	switch frame.Type {
+	case SettingFrameType:
+		settingFrame := SettingFrame{
+			AckFlag: FlagType(packet[4]) & SettingAckFlag,
+			Params:  map[SettingParam]uint32{},
+		}
+		for i := 0; i < int(frameLength)/(6); i++ {
+			_, err := reader.Read(packet[:6])
+			if err != nil {
+				return err
+			}
+			settingFrame.Params[SettingParam(binary.BigEndian.Uint16(packet[:2]))] = binary.BigEndian.Uint32(packet[2:6])
+		}
+		frame.Data = settingFrame
+		return nil
+	case HeaderFrameType:
+		headerFrame := HeaderFrame{
+			EndStreamFlag: FlagType(packet[4]) & HeaderEndStreamFlag,
+			EndHeaderFlag: FlagType(packet[4]) & HeaderEndHeaderFlag,
+			PaddingFlag:   FlagType(packet[4]) & HeaderPaddingFlag,
+			PriorityFlag:  FlagType(packet[4]) & HeaderPriorityFlag,
+		}
+		if headerFrame.PaddingFlag == HeaderPaddingFlag {
+			_, err := reader.Read(packet[:1])
+			if err != nil {
+				return err
+			}
+			headerFrame.PaddingLength = uint8(packet[0])
+		}
+		if headerFrame.PriorityFlag == HeaderPriorityFlag {
+			_, err := reader.Read(packet[:5])
+			if err != nil {
+				return err
+			}
+			headerFrame.StreamDependency = binary.BigEndian.Uint32(packet[:4])
+			headerFrame.Weight = uint8(packet[4])
+		}
+
+		if err := h.decoder.Decode(reader, &headerFrame.HeaderFields); err != nil {
+			return err
+		}
+
+		if headerFrame.PaddingLength != 0 {
+			n, err := reader.Read(packet[:headerFrame.PaddingLength])
+			if err != nil {
+				return err
+			}
+			if n != int(headerFrame.PaddingLength) {
+				return fmt.Errorf("not enough padding")
+			}
+		}
+
+		frame.Data = headerFrame
+		return nil
+	default:
+		return fmt.Errorf("unknown frame type")
+	}
+}
--- frame_test.go
+++ frame_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestSettingFrame(t *testing.T) {
+func TestEncodeSettingFrame(t *testing.T) {
 	expected := []byte{
 		0x00, 0x00, 0x12, 0x04,
 		0x00, 0x00, 0x00, 0x00,
@@ -30,7 +30,7 @@ func TestSettingFrame(t *testing.T) {
 
 	var buf = bytes.Buffer{}
 	handler := NewFrameHandler()
-	_, err := handler.Serialize(&buf, frame)
+	_, err := handler.Encode(&buf, frame)
 	if err != nil {
 		t.Error(err)
 	}
@@ -39,8 +39,55 @@ func TestSettingFrame(t *testing.T) {
 		t.Error("wrong")
 	}
 }
+func TestDecodeSettingFrame(t *testing.T) {
+	expectedFrame := Frame{
+		Type:     SettingFrameType,
+		StreamID: 0,
+		Data: SettingFrame{
+			AckFlag: UnsetFlag,
+			Params: map[SettingParam]uint32{
+				SettingsMaxConcurrentStreams: 100,
+				SettingsInitialWindowSize:    33554432,
+				SettingsEnablePush:           0,
+			},
+		},
+	}
+	rawFrame := []byte{
+		0x00, 0x00, 0x12, 0x04,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x03, 0x00,
+		0x00, 0x00, 0x64, 0x00,
+		0x04, 0x02, 0x00, 0x00,
+		0x00, 0x00, 0x02, 0x00,
+		0x00, 0x00, 0x00,
+	}
 
-func TestHeaderFrame(t *testing.T) {
+	frame := Frame{}
+	var buf = bytes.NewBuffer(rawFrame)
+	handler := NewFrameHandler()
+	err := handler.Decode(buf, &frame)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if frame.Type != expectedFrame.Type || frame.StreamID != expectedFrame.StreamID {
+		t.Errorf("not expected")
+	}
+
+	settingFrame := frame.Data.(SettingFrame)
+	expectedHeaderFrame := expectedFrame.Data.(SettingFrame)
+
+	if settingFrame.AckFlag != expectedHeaderFrame.AckFlag {
+		t.Errorf("excepted AckFlag flag: %d got %d", expectedHeaderFrame.AckFlag, settingFrame.AckFlag)
+	}
+	for key := range settingFrame.Params {
+		if settingFrame.Params[key] != expectedHeaderFrame.Params[key] {
+			t.Errorf("expected params: %d got %d", expectedHeaderFrame.Params[key], settingFrame.Params[key])
+		}
+	}
+}
+
+func TestEncodeHeaderFrame(t *testing.T) {
 	expected := []byte{
 		0x00, 0x00, 0x1a, 0x01,
 		0x05, 0x00, 0x00, 0x00,
@@ -65,7 +112,7 @@ func TestHeaderFrame(t *testing.T) {
 			PaddingLength:    0,
 			Weight:           0,
 
-			headerFields: []HeaderField{
+			HeaderFields: []HeaderField{
 				{name: ":method:", value: "GET"},
 				{name: ":path:", value: "/"},
 				{name: ":scheme:", value: "https"},
@@ -78,7 +125,7 @@ func TestHeaderFrame(t *testing.T) {
 
 	var buf = bytes.Buffer{}
 	handler := NewFrameHandler()
-	_, err := handler.Serialize(&buf, frame)
+	_, err := handler.Encode(&buf, frame)
 	if err != nil {
 		t.Error(err)
 	}
@@ -86,5 +133,87 @@ func TestHeaderFrame(t *testing.T) {
 	if !bytes.Equal(expected, buf.Bytes()) {
 		t.Errorf("expected:\n%v\ngot:\n%v\n", expected, buf.Bytes())
 		t.Error("wrong")
+	}
+}
+func TestDecodeHeaderFrame(t *testing.T) {
+	expectedFrame := Frame{
+		Type:     HeaderFrameType,
+		StreamID: 1,
+		Data: HeaderFrame{
+			EndStreamFlag: HeaderEndStreamFlag,
+			EndHeaderFlag: HeaderEndHeaderFlag,
+			PaddingFlag:   UnsetFlag,
+			PriorityFlag:  UnsetFlag,
+
+			StreamDependency: 0,
+			PaddingLength:    0,
+			Weight:           0,
+
+			HeaderFields: []HeaderField{
+				{name: ":method:", value: "GET"},
+				{name: ":path:", value: "/"},
+				{name: ":scheme:", value: "https"},
+				{name: ":authority:", value: "localhost"},
+				{name: "user-agent", value: "curl/7.85.0"},
+				{name: "accept", value: "*/*"},
+			},
+		},
+	}
+
+	rawFrame := []byte{
+		0x00, 0x00, 0x1a, 0x01,
+		0x05, 0x00, 0x00, 0x00,
+		0x01, 0x82, 0x84, 0x87,
+		0x41, 0x86, 0xa0, 0xe4,
+		0x1d, 0x13, 0x9d, 0x09,
+		0x7a, 0x88, 0x25, 0xb6,
+		0x50, 0xc3, 0xab, 0xbc,
+		0xda, 0xe0, 0x53, 0x03,
+		0x2a, 0x2f, 0x2a,
+	}
+
+	frame := Frame{}
+	var buf = bytes.NewBuffer(rawFrame)
+	handler := NewFrameHandler()
+	err := handler.Decode(buf, &frame)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if frame.Type != expectedFrame.Type || frame.StreamID != expectedFrame.StreamID {
+		t.Errorf("not expected")
+	}
+
+	headerFrame := frame.Data.(HeaderFrame)
+	expectedHeaderFrame := expectedFrame.Data.(HeaderFrame)
+
+	if headerFrame.EndHeaderFlag != expectedHeaderFrame.EndHeaderFlag {
+		t.Errorf("excepted EndHeaderFlag flag: %d got %d", expectedHeaderFrame.EndHeaderFlag, headerFrame.EndHeaderFlag)
+	}
+	if headerFrame.EndStreamFlag != expectedHeaderFrame.EndStreamFlag {
+		t.Errorf("excepted EndStreamFlag flag: %d got %d", expectedHeaderFrame.EndStreamFlag, headerFrame.EndStreamFlag)
+	}
+	if headerFrame.PaddingFlag != expectedHeaderFrame.PaddingFlag {
+		t.Errorf("excepted PaddingFlag flag: %d got %d", expectedHeaderFrame.PaddingFlag, headerFrame.PaddingFlag)
+	}
+	if headerFrame.PriorityFlag != expectedHeaderFrame.PriorityFlag {
+		t.Errorf("excepted PriorityFlag flag: %d got %d", expectedHeaderFrame.PriorityFlag, headerFrame.PriorityFlag)
+	}
+	if headerFrame.StreamDependency != expectedHeaderFrame.StreamDependency {
+		t.Errorf("excepted StreamDependency flag: %d got %d", expectedHeaderFrame.StreamDependency, headerFrame.StreamDependency)
+	}
+	if headerFrame.PaddingLength != expectedHeaderFrame.PaddingLength {
+		t.Errorf("excepted padding length: %d got %d", expectedHeaderFrame.PaddingLength, headerFrame.PaddingLength)
+	}
+	if headerFrame.Weight != expectedHeaderFrame.Weight {
+		t.Errorf("excepted weight: %d got %d", expectedHeaderFrame.Weight, headerFrame.Weight)
+	}
+	for i := 0; i < len(headerFrame.HeaderFields); i++ {
+		if headerFrame.HeaderFields[i].name != expectedHeaderFrame.HeaderFields[i].name {
+			t.Errorf("expected header field name: %s got %s", expectedHeaderFrame.HeaderFields[i].name, headerFrame.HeaderFields[i].name)
+		}
+		if headerFrame.HeaderFields[i].value != expectedHeaderFrame.HeaderFields[i].value {
+			t.Errorf("expected header field value: %s got %s", expectedHeaderFrame.HeaderFields[i].value, headerFrame.HeaderFields[i].value)
+		}
 	}
 }
