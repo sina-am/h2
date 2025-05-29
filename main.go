3d44b2e754bdd5f8da4503cb202b14c29f328347
sina.a.m <sinaaarabi2@gmail.com>
2025-05-29 23:09:37 +0330

    adds example in main.go

--- main.go
+++ main.go
@@ -1,211 +1,117 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
-	"errors"
+	"crypto/tls"
 	"fmt"
-	"io"
 	"log"
+	"net"
 )
 
-var (
-	ErrProtocol = errors.New("protocol error")
-)
-
-type (
-	FrameType    uint
-	FlagType     uint8
-	SettingParam uint16
-)
-
-const (
-	DataFrameType    FrameType = 0x00
-	HeaderFrameType  FrameType = 0x01
-	SettingFrameType FrameType = 0x04
-
-	UnsetFlag           FlagType = 0x0
-	SettingAckFlag      FlagType = 0x1
-	HeaderEndStreamFlag FlagType = 0x1
-	HeaderEndHeaderFlag FlagType = 0x4
-	HeaderPaddingFlag   FlagType = 0x8
-	HeaderPriorityFlag  FlagType = 0x20
-
-	SettingsHeaderTableSize      SettingParam = 1 // 4,096
-	SettingsEnablePush           SettingParam = 2 // true
-	SettingsMaxConcurrentStreams SettingParam = 3 // no less than 100
-	SettingsInitialWindowSize    SettingParam = 4 // 65,535
-	SettingsMaxFrameSize         SettingParam = 5 // 16,384
-	SettingsMaxHeaderListSize    SettingParam = 6 // unlimited
-)
-
-/*
-All frames begin with a fixed 9-octet header followed by a variable-
-length payload.
-
-	+-----------------------------------------------+
-	|                 Length (24)                   |
-	+---------------+---------------+---------------+
-	|   Type (8)    |   Flags (8)   |
-	+-+-------------+---------------+-------------------------------+
-	|R|                 Stream Identifier (31)                      |
-	+=+=============================================================+
-	|                   Frame Payload (0...)                      ...
-	+---------------------------------------------------------------+
-
-	                      Figure 1: Frame Layout
-*/
-type Frame struct {
-	Type     FrameType
-	StreamID uint32
-	Data     any
-}
-
-/*
-The payload of a SETTINGS frame consists of zero or more parameters,
-each consisting of an unsigned 16-bit setting identifier and an
-unsigned 32-bit value.
-
-	+-------------------------------+
-	|       Identifier (16)         |
-	+-------------------------------+-------------------------------+
-	|                        Value (32)                             |
-	+---------------------------------------------------------------+
-
-	                     Figure 10: Setting Format
-*/
-type SettingFrame struct {
-	AckFlag FlagType
-	Params  map[SettingParam]uint32
-}
-
-/*
-Headers frame structure
-
-	+---------------+
-	|Pad Length? (8)|
-	+-+-------------+-----------------------------------------------+
-	|E|                 Stream Dependency? (31)                     |
-	+-+-------------+-----------------------------------------------+
-	|  Weight? (8)  |
-	+-+-------------+-----------------------------------------------+
-	|                   Header Block Fragment (*)                 ...
-	+---------------------------------------------------------------+
-	|                           Padding (*)                       ...
-	+---------------------------------------------------------------+
-
-	Figure 7: HEADERS Frame Payload
-*/
-type HeaderFrame struct {
-	EndStreamFlag FlagType
-	EndHeaderFlag FlagType
-	PaddingFlag   FlagType
-	PriorityFlag  FlagType
-
-	StreamDependency uint32
-	PaddingLength    uint8
-	Weight           uint8
-
-	headerFields []HeaderField
-}
-
-type frameHandler struct {
-	encoder HPackEncoder
-}
-
-func NewFrameHandler() *frameHandler {
-	return &frameHandler{
-		encoder: NewHPackEncoder(),
-	}
-}
-
-func (h *frameHandler) Serialize(writer io.Writer, frame Frame) (int, error) {
-	switch frame.Type {
-	case SettingFrameType:
-		settingFrame, ok := frame.Data.(SettingFrame)
-		if !ok {
-			return 0, fmt.Errorf("invalid data")
-		}
-
-		length := len(settingFrame.Params) * 6
-
-		packet := make([]byte, 9+length)
-		// Length (24)
-		packet[0] = byte((length >> 16) & 0xFF)
-		packet[1] = byte((length >> 8) & 0xFF)
-		packet[2] = byte(length & 0xFF)
-		packet[3] = byte(SettingFrameType)                     // Type (8)
-		packet[4] = byte(settingFrame.AckFlag)                 // Flags (8)
-		binary.BigEndian.PutUint32(packet[5:], frame.StreamID) // StreamID (32)
-
-		index := 9
-		for identifier, value := range settingFrame.Params {
-			binary.BigEndian.PutUint16(packet[index:], uint16(identifier)) // 9-11
-			binary.BigEndian.PutUint32(packet[index+2:], value)            // 11-15
-
-			index += 6
-		}
-
-		return writer.Write(packet)
-	case HeaderFrameType:
-		headerFrame, ok := frame.Data.(HeaderFrame)
-		if !ok {
-			return 0, fmt.Errorf("invalid data")
-		}
-
-		packet := make([]byte, 9)
-		packet[3] = byte(HeaderFrameType) // Type (8)
-		packet[4] = byte(
-			headerFrame.EndStreamFlag |
-				headerFrame.EndHeaderFlag |
-				headerFrame.PaddingFlag |
-				headerFrame.PriorityFlag,
-		) // Flags (8)
-		binary.BigEndian.PutUint32(packet[5:], frame.StreamID) // StreamID (32)
-
-		if headerFrame.PaddingFlag != UnsetFlag {
-			packet = append(packet, headerFrame.PaddingLength)
-		}
-		if headerFrame.PriorityFlag != UnsetFlag {
-			packet = binary.BigEndian.AppendUint32(packet, headerFrame.StreamDependency)
-			packet = append(packet, headerFrame.Weight)
-		}
-
-		buf := bytes.Buffer{}
-		_, err := h.encoder.Encode(&buf, headerFrame.headerFields)
-		if err != nil {
-			return 0, err
-		}
-
-		packet = append(packet, buf.Bytes()...)
-		length := len(packet) - 9
-		packet[0] = byte((length >> 16) & 0xFF)
-		packet[1] = byte((length >> 8) & 0xFF)
-		packet[2] = byte(length & 0xFF)
-
-		return writer.Write(packet)
-	default:
-		return 0, fmt.Errorf("invalid frame type")
-	}
-}
 func main() {
-	frame := Frame{
+	serverAddr := "127.0.0.1:443"
+
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"h2", "http/1.1"},
+	}
+
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	tlsConn := tls.Client(conn, tlsConfig)
+
+	if err := tlsConn.Handshake(); err != nil {
+		log.Fatalf("TLS handshake failed: %v", err)
+	}
+
+	fmt.Println("TLS connection established successfully")
+
+	handler := NewFrameHandler()
+
+	settingFrame := Frame{
 		Type:     SettingFrameType,
 		StreamID: 0,
+		Flags:    UnsetFlag,
 		Data: any(SettingFrame{
-			AckFlag: SettingAckFlag,
 			Params: map[SettingParam]uint32{
-				SettingsMaxFrameSize: 1024,
+				SettingsMaxConcurrentStreams: 100,
+				SettingsInitialWindowSize:    33554432,
+				SettingsEnablePush:           0,
+			},
+		}),
+	}
+	headerFrame := Frame{
+		Type:     HeaderFrameType,
+		StreamID: 1,
+		Flags:    EndStreamFlag | EndHeaderFlag,
+		Data: any(HeaderFrame{
+			HeaderFields: []HeaderField{
+				{name: ":method:", value: "GET"},
+				{name: ":path:", value: "/"},
+				{name: ":scheme:", value: "https"},
+				{name: ":authority:", value: "localhost"},
+				{name: "user-agent", value: "go/h2"},
+				{name: "accept", value: "*/*"},
 			},
 		}),
 	}
 
-	buf := bytes.Buffer{}
-	handler := NewFrameHandler()
-	bytes, err := handler.Serialize(&buf, frame)
-	if err != nil {
-		log.Fatal((err))
+	ackSettingFrame := Frame{
+		Type:     SettingFrameType,
+		StreamID: 0,
+		Flags:    AckFlag,
+		Data: any(SettingFrame{
+			Params: map[SettingParam]uint32{},
+		}),
 	}
 
-	fmt.Printf("%v\n", bytes)
+	// Send Magic
+	_, err = tlsConn.Write([]byte("\x50\x52\x49\x20\x2a\x20\x48\x54\x54\x50\x2f\x32\x2e\x30\x0d\x0a\x0d\x0a\x53\x4d\x0d\x0a\x0d\x0a"))
+	if err != nil {
+		log.Fatalf("Failed to send request: %v", err)
+	}
+
+	_, err = handler.Encode(tlsConn, settingFrame)
+	if err != nil {
+		log.Fatalf("Failed to send request: %v", err)
+	}
+
+	_, err = handler.Encode(tlsConn, headerFrame)
+	if err != nil {
+		log.Fatalf("Failed to send request: %v", err)
+	}
+
+	_, err = handler.Encode(tlsConn, ackSettingFrame)
+	if err != nil {
+		log.Fatalf("Failed to send request: %v", err)
+	}
+
+	frame := Frame{}
+	if err := handler.Decode(tlsConn, &frame); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(frame)
+	frame = Frame{}
+	if err := handler.Decode(tlsConn, &frame); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(frame)
+	frame = Frame{}
+	if err := handler.Decode(tlsConn, &frame); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(frame)
+	frame = Frame{}
+	if err := handler.Decode(tlsConn, &frame); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(frame)
+	frame = Frame{}
+	if err := handler.Decode(tlsConn, &frame); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(frame)
 }
